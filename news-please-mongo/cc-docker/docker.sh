027e025d7b93e5d26187c855e1bb9ddbfd42c1da
ssdorsey <spencer.dorsey@duke.edu>
2020-05-11 13:24:50 -0400

    cleaning unwanted files

--- news-please-mongo/cc-docker/docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# output is written to file, process runs in foreground so container doesn't terminate
-news-please-cc /npdata/warcs /npdata/articles keep 32 >> /npdata/all.log 2>&1
--- news-please-mongo/docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /news-please/newsplease
-python3 __main__.py -c ./config
