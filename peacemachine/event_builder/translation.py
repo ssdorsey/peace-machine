13a84fc2c6b303282fe1f224d07602c37dc41d49
Akanksha Bhattacharyya <akanksha.nb@gmail.com>
2020-09-15 21:40:00 -0400

    non-duplicated, find-one, match on url by original data

--- peacemachine/event_builder/translation.py
+++ peacemachine/event_builder/translation.py
@@ -17,17 +17,15 @@ def modify_url(missing):
     :param missing: leftover urls not found
     '''
     modified = []
-    for i in missing:
-        url = i[0]
-        title = i[1]
+    for url in missing:
         url = url.replace('http://', '')
         url = url.replace('https://', '')
         url = url.replace('www.', '')
         url = url[:-1]
-        modified += [(url,title.strip())]
+        modified.append(url)
     return modified
 
-def get_data(urls, titles, source):
+def get_data(modified, titles, source):
     '''
     :param source: source domain
     :param urls: urls to search
@@ -35,17 +33,17 @@ def get_data(urls, titles, source):
     '''
     missing = []
     item = []
-    for url,title in zip(urls, titles):
-        data = [i for i in db.articles.find(
+    for url in modified:
+        data = db.articles.find_one(
             {
                 'source_domain': source,
-                'url' : url
+                'url' : {'$regex': url}
             }
-        )]
-        if len(data)==0:
-            missing.append((url,title))
+        )
+        if data==None:
+            missing.append(url)
         else:
-            item.append(data)
+            item.append((data,url))
     return item, missing  
     
 def get_data1(modified, source):
@@ -56,7 +54,7 @@ def get_data1(modified, source):
     item = []
     missing = []
     for url,title in modified:
-        data = [i for i in db.articles.find(
+        data = [i for i in db.articles.find_one(
             {
                 'source_domain': source,
                 'url' : {'$regex' : url}
@@ -76,7 +74,7 @@ def match_title(still_missing, source):
     item = []
     missing = []
     for url,title in still_missing:
-        data = [i for i in db.articles.find(
+        data = [i for i in db.articles.find_one(
                 {
                     'source_domain': source,
                     'title' : { '$regex': title}
@@ -118,47 +116,66 @@ def get_text(i, translator):
     return new_dict
 
 # %%
-df = pd.read_csv('/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/insajder.csv')
+df = pd.read_csv('/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/danas_updated.csv')
 df = df.dropna(subset=['URL'])
 urls = df['URL']
 titles = df['Title']
-source = 'insajder.net'
+source = 'danas.rs'
 
-item, missing = get_data(urls, titles, source)
-modified = modify_url(missing)
-item1, missing1 = get_data1(modified, source)
-item2, missing2 = match_title(missing1, source)
+modified = modify_url(urls)
+#%%
+item, missing = get_data(modified, titles, source)
+# item1, missing1 = get_data1(modified, source)
+# item2, missing2 = match_title(missing1, source)
+# flatten = [i for sublist in hold for i in sublist]
 
-hold = []
-hold = item + item1 + item2
-flatten = [i for sublist in hold for i in sublist]
-
+#%%
 not_translated = []
 translated = []
-for i in flatten:
+for i in item:
     translator = Translator()
-    if i['maintext'] is None:
-        if i['description'] is None:
+    if i[0]['maintext'] is None:
+        if i[0]['description'] is None:
             not_translated.append(i)
         else:
-            translated.append(get_description(i, translator))
+            translated.append((get_description(i[0], translator), i[1]))
     else:
-        translated.append(get_text(i, translator))
+        translated.append((get_text(i[0], translator), i[1]))
 
 
 # %%
 import csv
-path = '/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/insajder_trans.csv'
-df = pd.DataFrame(columns = ['date_publish', 'description','title','source_domain','maintext','url','translated_text','translated_title']) 
+path = '/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/danas_updated_trans.csv'
+df = pd.DataFrame(columns = ['date_publish', 'source_domain','URL','Title','translated_title','description','maintext','translated_text']) 
 for i in translated:
-    new_row = {'date_publish': i['date_publish'], 'description': i['description'], 'title':i['title'], 'source_domain': i['source_domain'], 'maintext':i['maintext'],'url':i['url'], 'translated_text':i['translated_text'],'translated_title':i['translated_title']}
+    new_row = {'date_publish': i[0]['date_publish'], 'description': i[0]['description'], 'Title':i[0]['title'], 'source_domain': i[0]['source_domain'], 'maintext':i[0]['maintext'],'URL':i[1], 'translated_text':i[0]['translated_text'],'translated_title':i[0]['translated_title']}
     df = df.append(new_row, ignore_index=True)
 df.to_csv(path, index=False)
 
+    
 # %%
-x = [h['translated_text'] for h in translated]
+orgPath = '/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/danas_updated.csv'
+path2Merge = '/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/danas_updated_trans.csv'
+df1 = pd.read_csv(orgPath)
+df1 = df1.dropna(subset=['URL'])
+df1['URL'] = modify_url(df1['URL'])
+df2 = pd.read_csv(path2Merge)
+df_inner = pd.merge(df1, df2, on='URL', how='left')
+
 #%%
-import json
-with open('/Users/akankshabhattacharyya/Documents/DukePeaceProject/translated3.txt','w') as f:
-    for i in x:
-        f.write("%s\n" % i)
+# reqdColumns = ['URL', 'source_domain', 'translated_title', 'maintext','description','translated_text','civic_event0','civic_event1','civic_event2','RAI_Event0','RAI_Event1','RAI_Event2','civic_code_in_db']
+df_inner1 = df_inner[reqdColumns]
+# %%
+new_path = '/Users/akankshabhattacharyya/Documents/DukePeaceProject/CSV/danasTrans_updated.csv'
+df_inner.to_csv(new_path, index=False)
+# %%
+len(item)
+# %%
+def modify(urls):
+    for url in urls:
+        url = url.replace('http://', '')
+        url = url.replace('https://', '')
+        url = url.replace('www.', '')
+        url = url[:-1]
+    return urls
+# %%
